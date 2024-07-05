9c60bc004a375747b0529f66e8bcbfc9ec9e6c8c
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-05 15:54:33 +0900

    completed quiz-app

--- quiz-app/app.js
+++ quiz-app/app.js
@@ -1,25 +1,71 @@
-const question = 'q1?';
-const answers = ['opt1', 'opt2', 'opt3', 'opt4'];
-const correct = 'opt3';
+const quiz = [
+    {
+        question: 'q1',
+        answers: [
+            'opt1', 
+            'opt2', 
+            'opt3', 
+            'opt4',
+        ],
+        correct: 'opt3'
+    }, 
+    {
+        question: 'q2',
+        answers: [
+            'opt1', 
+            'opt2', 
+            'opt3', 
+            'opt4',
+        ],
+        correct: 'opt1'
+
+    }, 
+    {
+        question: 'q3',
+        answers: [
+            'opt1', 
+            'opt2', 
+            'opt3', 
+            'opt4',
+        ],
+        correct: 'opt3'
+
+    }
+];
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
 
 const $button = document.getElementsByTagName('button');
+
 const setupQuiz = () => {
-    document.getElementById('js-question').textContent = question;
+    document.getElementById('js-question').textContent = quiz[quizIndex].question;
     // HTMLのオブジェクトを格納する変数には、先頭に$をつける
     let buttonIndex = 0;
     while (buttonIndex < $button.length) {
-        $button[buttonIndex].textContent = answers[buttonIndex];
+        $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
         buttonIndex++;
     }
      
 }
+
 setupQuiz();
 
 const clichHandler = (e) => {
-    if (correct === e.target.textContent) {
+    if (quiz[quizIndex].correct === e.target.textContent) {
         window.alert('correct');
+        score++;
     } else {
         window.alert('incorrect');
+    }
+
+    quizIndex++;
+
+    if (quizIndex < quizLength) {
+        setupQuiz();
+    } else {
+        window.alert('終了!あなたの正解数は' + score + '/' + quizLength + 'です');
     }
 };
 
