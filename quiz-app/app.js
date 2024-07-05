33bde6336b7076ac2df85173ddfb70eb736b9baa
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-05 12:30:02 +0900

    modified app.js

--- quiz-app/app.js
+++ quiz-app/app.js
@@ -2,3 +2,31 @@ const question = 'q1?';
 const answers = ['opt1', 'opt2', 'opt3', 'opt4'];
 const correct = 'opt3';
 
+const $button = document.getElementsByTagName('button');
+const setupQuiz = () => {
+    document.getElementById('js-question').textContent = question;
+    // HTMLのオブジェクトを格納する変数には、先頭に$をつける
+    let buttonIndex = 0;
+    while (buttonIndex < $button.length) {
+        $button[buttonIndex].textContent = answers[buttonIndex];
+        buttonIndex++;
+    }
+     
+}
+setupQuiz();
+
+const clichHandler = (e) => {
+    if (correct === e.target.textContent) {
+        window.alert('correct');
+    } else {
+        window.alert('incorrect');
+    }
+};
+
+let handlerIndex = 0;
+while (handlerIndex < $button.length) {
+    $button[handlerIndex].addEventListener('click', (e) => {
+        clichHandler(e);  
+    });  
+    handlerIndex++;
+};
