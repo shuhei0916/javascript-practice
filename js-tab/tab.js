2933eca80748df55059dcc2f2c2dc58e6e90a89e
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-06 13:31:59 +0900

    added handlerClick function in tab.js

--- js-tab/tab.js
+++ js-tab/tab.js
@@ -16,6 +16,28 @@
     }
     init();
 
+    const handlerClick = (e) => {
+        e.preventDefault(); // tabのaタブ（アンカーリンク：別ページに遷移するためのジャンプリンク）の要素を殺す
+        // console.log('Clicked!');
+        // console.log(e);
+
+        // クリックされたnavとそのdataを取得
+        const $this = e.target; 
+        const targetVal = $this.dataset.nav; // dataset.（属性名)でデータ属性の値が取得できる
+        console.log('$targetval', targetVal);
+
+
+        // 対象のコンテンツをアクティブ化する
+        $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display ='block';
+        
+    };
     
+    // 全nav要素に対して関数を適用
+    let index = 0; 
+    while(index < $nav.length) {
+        $nav[index].addEventListener('click', (e) => handlerClick(e));
+        index++;
+    }
+
 
 })();
