13707850603e4d5b7356a52d1a24c5e1e7ffabbf
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-06 13:12:13 +0900

    defined init in tab.js

--- js-tab/tab.js
+++ js-tab/tab.js
@@ -1,6 +1,7 @@
 
 // グローバル変数の汚染を防ぐための即時関数
 (() => {
+    // 変数名に$をつけてDOM要素であることを示す
     const $doc = document;
     const $tab = $doc.getElementById('js-tab');
     const $nav = $tab.querySelectorAll('[data-nav]');
@@ -8,5 +9,13 @@
 
     console.log("hehe", $nav);
     console.log($tab);
+    console.log('$content', $content);
+
+    const init = () => {
+        $content[0].style.display = 'block';
+    }
+    init();
+
+    
 
 })();
