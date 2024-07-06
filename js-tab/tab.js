73c2b1e4619f8f6258ad2fe40bf622a6e8edafdd
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-06 15:13:00 +0900

    completed js-tab

--- js-tab/tab.js
+++ js-tab/tab.js
@@ -6,6 +6,8 @@
     const $tab = $doc.getElementById('js-tab');
     const $nav = $tab.querySelectorAll('[data-nav]');
     const $content = $tab.querySelectorAll('[data-content]');
+    const ACTIVE_CLASS = 'is-active';
+    const navLen = $nav.length;
 
     console.log("hehe", $nav);
     console.log($tab);
@@ -18,23 +20,28 @@
 
     const handlerClick = (e) => {
         e.preventDefault(); // tabのaタブ（アンカーリンク：別ページに遷移するためのジャンプリンク）の要素を殺す
-        // console.log('Clicked!');
-        // console.log(e);
 
         // クリックされたnavとそのdataを取得
         const $this = e.target; 
         const targetVal = $this.dataset.nav; // dataset.（属性名)でデータ属性の値が取得できる
-        console.log('$targetval', targetVal);
-
+        
+        // 対象外のタブコンテンツをリセットする
+        let index = 0;
+        while(index < navLen) {
+            $content[index].style.display = 'none';
+            $nav[index].classList.remove(ACTIVE_CLASS);
+            index++;
+        }
 
         // 対象のコンテンツをアクティブ化する
         $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display ='block';
-        
+        $nav[targetVal].classList.add(ACTIVE_CLASS);
+        console.log($nav[targetVal].classList);
     };
     
     // 全nav要素に対して関数を適用
     let index = 0; 
-    while(index < $nav.length) {
+    while(index < navLen) {
         $nav[index].addEventListener('click', (e) => handlerClick(e));
         index++;
     }
