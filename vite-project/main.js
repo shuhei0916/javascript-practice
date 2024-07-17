2460503060c9e24bbb96fe39d44e7bb2304641a0
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-17 18:55:32 +0900

    modified main.js and style.css

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,24 +1,17 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!!!!!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import Splide from '@splidejs/splide';
 
-setupCounter(document.querySelector('#counter'))
+// デフォルトのテーマ
+import '@splidejs/splide/css';
+
+
+// または、そのほかのテーマ
+import '@splidejs/splide/css/skyblue';
+import '@splidejs/splide/css/sea-green';
+
+
+// あるいは、コアスタイルのみ
+import '@splidejs/splide/css/core';
+
+new Splide( '.splide' ).mount();
