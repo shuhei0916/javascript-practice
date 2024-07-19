59cf4ce8750dfdf615dfa9861abab21947789aa2
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-19 16:45:55 +0900

    first refactoring phase of isLeapYear TDD

--- vite-project/test.js
+++ vite-project/test.js
@@ -4,8 +4,6 @@ import isLeapYear from './isLeapYear.js'
 describe('isLeapYear', function () {
   it('should return true for a year divisible by 4', function () {
     const year = 2024;
-    const actual = isLeapYear(year);
-    const expected = true;
-    assert.equal(actual, expected);
+    assert.equal(isLeapYear(year), true);
   });
 });
