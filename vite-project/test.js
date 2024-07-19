d9659fbd44729e313474725fd039e7ad42166d97
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-19 16:44:29 +0900

    the first green of isLeapYear TDD

--- vite-project/test.js
+++ vite-project/test.js
@@ -1,7 +1,11 @@
 import assert from 'assert';
+import isLeapYear from './isLeapYear.js'
 
 describe('isLeapYear', function () {
-  it('これは失敗するはず', function () {
-    assert.fail('失敗');
+  it('should return true for a year divisible by 4', function () {
+    const year = 2024;
+    const actual = isLeapYear(year);
+    const expected = true;
+    assert.equal(actual, expected);
   });
 });
