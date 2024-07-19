28925520da3ff5670dea276ca8570eb5e0118930
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-19 16:26:32 +0900

    the first red of isLeapYear TDD

--- vite-project/test.js
+++ vite-project/test.js
@@ -1,9 +1,7 @@
 import assert from 'assert';
 
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+describe('isLeapYear', function () {
+  it('これは失敗するはず', function () {
+    assert.fail('失敗');
   });
 });
