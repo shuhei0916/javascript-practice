34137b4900872a4ef5902852921de5ad63f5fd4f
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-19 17:18:19 +0900

    added year 1900 test case and its product code

--- vite-project/isLeapYear.js
+++ vite-project/isLeapYear.js
@@ -1,3 +1,11 @@
-export default function isLeapYear() {
-    return true;
+export default function isLeapYear(year) {
+    if (year % 4 !== 0) {
+        return false;
+    } else if (year % 100 !== 0) {
+        return true;
+    } else if (year % 400 !== 0) {
+        return false;
+    } else {
+        return true;
+    }
 }
--- vite-project/test.js
+++ vite-project/test.js
@@ -2,8 +2,19 @@ import assert from 'assert';
 import isLeapYear from './isLeapYear.js'
 
 describe('isLeapYear', function () {
-  it('should return true for a year divisible by 4', function () {
-    const year = 2024;
-    assert.equal(isLeapYear(year), true);
+  it('should return true for 2024', function () {
+    const year1 = 2024;
+    assert.equal(isLeapYear(year1), true);
   });
+
+  it ('should return false for a year divivible by 100', function () {
+    const year = 1900;
+    assert.equal(isLeapYear(year), false);
+  });
+  
+  it('should return false for 2025', function () {
+    const year2 = 2025;
+    assert.equal(isLeapYear(year2), false);
+  });
+
 });
